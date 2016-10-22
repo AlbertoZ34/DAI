9acae90dd0a0958640c2391ebf8b813009a73edb
ALBERTO ZAINOS MALDONADO <azainosm@itam.mx>
2016-10-21 21:41:22 -0500

    Alta y baja hecha

---
+++ Ejercicio!/Ejercicio!/Conexion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio_
+{
+    class Conexion
+    {
+       SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public static SqlConnection agregaConexion() {
+            SqlConnection cnn;
+            String query = "Data Source=112SALAS05;Initial Catalog=Ejercicio!;Persist Security Info=True;User ID=sa;Password=sqladmin";
+            cnn = new SqlConnection(query);
+            cnn.Open();
+            //MessageBox.Show("Conexión exitosa");
+            return cnn;
+        }
+
+
+
+
+
+
+        public void llenarComboPrograma(ComboBox cb)
+        {
+            try
+            {
+                String query = "select nombre from programas";
+                cmd = new SqlCommand(query,agregaConexion());
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read()) {
+                    cb.Items.Add(dr["nombre"].ToString());
+                }
+                cb.SelectedIndex = 0;
+                dr.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Llenado incorrecto: \n"+e.Message);
+            }
+
+        }
+
+
+
+
+        internal void llenarComboAlumnos(ComboBox cbAlumnos)
+        {
+            try
+            {
+                String query = "select nombre from Aspirante1";
+                cmd = new SqlCommand(query, agregaConexion());
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    cbAlumnos.Items.Add(dr["nombre"].ToString());
+                }
+                cbAlumnos.SelectedIndex = 0;
+                dr.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Llenado incorrecto: \n" + e.Message);
+            }
+        }
+    }
+}
--- Ejercicio!/Ejercicio!/MainWindow.xaml.cs
+++ Ejercicio!/Ejercicio!/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,92 @@ namespace Ejercicio_
         {
             InitializeComponent();
         }
+
+        private void Grid_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            //Decirle a la conexion que llene el combo
+            Conexion con1 = new Conexion();
+            con1.llenarComboPrograma(cbInges);
+
+            con1.llenarComboAlumnos(cbAlumnos);
+        }
+
+        private void btAlta_Click(object sender, RoutedEventArgs e)
+        {
+            String sexo, nombre, grado, correo, fecha, inge;
+            //
+            if (tbNombre.Text != "" && tbSexo.Text != "" && tbCorreo.Text != "" && tbGrado.Text != "" && dpFechaN.SelectedDate.ToString() != "") {
+                nombre = tbNombre.Text;
+                sexo = tbSexo.Text;
+                grado = tbGrado.Text;
+                correo = tbCorreo.Text;
+                fecha = dpFechaN.Text;
+                inge = cbInges.Text;
+
+                try
+                {   //traemos una conexion
+                    SqlConnection con2 = Conexion.agregaConexion();
+                    String query = String.Format("Insert into Aspirante1 Values('{0}','{1}','{2}','{3}','{4}','{5}')",nombre,sexo,fecha,grado,correo,inge);
+                    SqlCommand cmd= new SqlCommand(query,con2);
+                    int res = cmd.ExecuteNonQuery();
+                    con2.Close();
+
+                    if (res > 0) {
+                        MessageBox.Show("Alta exitosa!");
+                    
+                    }else
+                        MessageBox.Show("Alta no exitosa!");
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se hizo la coneccion: \n"+ex.Message);
+                    
+                }
+                
+                
+
+            }
+        }
+
+
+        private void btBaja_Click(object sender, RoutedEventArgs e)
+        {
+            String nombre;
+
+            if (cbAlumnos.Text != "") {
+                nombre = cbAlumnos.Text;
+                try
+                {
+                     SqlConnection cnn = Conexion.agregaConexion();
+                     String query = "Delete from Aspirante1 WHERE Aspirante1.nombre='"+nombre+"'";
+                     SqlCommand cmd = new SqlCommand(query, cnn);
+                     int res = cmd.ExecuteNonQuery();
+                     cnn.Close();
+                     if (res > 0)
+                     {
+                        MessageBox.Show("Baja exitosa!");
+                     }
+                     else
+                        MessageBox.Show("Alta no exitosa!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se hizo la coneccion: \n" + ex.Message);
+                    
+                }
+               
+            
+            }
+
+            
+
+
+
+        }
+
+        
     }
 }
